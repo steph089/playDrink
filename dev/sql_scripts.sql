c889ec670cdbd1add9658a366c928f7166e4a187
Chris Marshall <chris@psinnovations>
2009-10-12 23:13:48 -0400

    added turns SQL scripts

--- dev/sql_scripts.sql
+++ dev/sql_scripts.sql
@@ -11,3 +11,25 @@ ALTER TABLE `games` ADD `player_id` INT NULL DEFAULT NULL ,
 ADD `dealer_id` INT NULL DEFAULT NULL;
 
  ALTER TABLE `players` DROP `modifier` ;
+ 
+ 
+ /*****************************************/
+ //turns
+ 
+ ALTER TABLE `games` ADD `turn_id` INT NOT NULL DEFAULT '0';
+ 
+  CREATE TABLE `playdrink`.`turns` (
+`id` INT NOT NULL AUTO_INCREMENT ,
+`player_id` INT NOT NULL ,
+`dealer_id` INT NOT NULL ,
+`first_guess_num` INT NULL ,
+`first_guess_result` VARCHAR( 255 ) NULL ,
+`second_guess_num` INT NULL ,
+`drinker_id` INT NULL ,
+`drinks` INT NULL ,
+`gets` INT NOT NULL ,
+`card` VARCHAR( 255 ) NULL ,
+PRIMARY KEY ( `id` ) ,
+INDEX ( `player_id` , `dealer_id` )
+) ENGINE = InnoDB ;
+ 
